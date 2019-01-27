9c25b488d14d24702f26e4efda2eb11e619b316c
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-01-28 01:04:14 +0300

    chore(webpack4 setup) : Use Webpack 4 to start a rect app from scratch
    - Npm init after cloning the directory
    - Npm install relevant files and depenecies as well as devDependencies
    
    [Delivers #163505256]

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Deal Safi!</h1>
+      </div>
+    );
+    
+  }
+}
